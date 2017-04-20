e8f2f35155cf49602f5ff5ebb59e30d5e808e425
John Blackwood <john@xnor.ca>
2017-04-20 14:22:38 +0000

    Changed voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Beer"
+optionB = "Rum"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
